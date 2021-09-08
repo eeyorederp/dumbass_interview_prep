3ee28fa4018f1ad692cb0ac9241e9f1a5f7dc390
Dhruv Rastogi <2dhruvrastogi@gmail.com>
2021-09-07 21:10:20 -0700

    grammy

---
+++ bot_242.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        
+        letters = [0] * 26
+        for c in s:
+            letters[ord(c) - 97] += 1
+        for c in t:
+            letters[ord(c) - 97] -= 1
+            if letters[ord(c) - 97] < 0:
+                return False
+        return True
